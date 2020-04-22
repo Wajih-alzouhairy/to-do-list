4651478187e5d627906938eec6bc4abf5a2948de
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-04-22 14:16:59 +0200

    22-04

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const addToList = () => {
+  let userInput = document.querySelector("#userText").value;
+  console.log(userInput);
+  let newLi = document.createElement("li");
+  let text = document.createTextNode(userInput);
+  newLi.appendChild(text);
+  document.querySelector(".result").appendChild(newLi);
+  document.querySelector("#userText").value = "";
+};
+let font = document.querySelector(".fontSize");
+
+font.style.fontSize = "2rem";
+font.style.color = "white";
