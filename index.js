321a08d24587d30276cab76b8a1e39a9e601bdab
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-04-22 16:45:53 +0200

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+const generateColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
+  }
+  color += "50";
+  return color;
+};
+
 const addToList = () => {
   let userInput = document.querySelector("#userText").value;
-  console.log(userInput);
-  let newLi = document.createElement("li");
-  let text = document.createTextNode(userInput);
-  newLi.appendChild(text);
-  document.querySelector(".result").appendChild(newLi);
-  document.querySelector("#userText").value = "";
+  console.log(userInput.length);
+  if (userInput.length > 0) {
+    console.log(userInput);
+    let newLi = document.createElement("li");
+    newLi.style.color = `${generateColor()}`;
+    let text = document.createTextNode(userInput);
+    newLi.appendChild(text);
+    document.querySelector(".result").appendChild(newLi);
+    document.querySelector("#userText").value = "";
+  }
 };
-let font = document.querySelector(".fontSize");
-
-font.style.fontSize = "2rem";
-font.style.color = "white";
